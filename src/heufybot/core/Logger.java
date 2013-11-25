544be39534aaebef002a590d0635b278c9b751ff
Heufneutje <stefan.frijters@hotmail.com>
2013-11-25 19:55:26 +0100

    Added simple logging to a file

--- src/heufybot/core/Logger.java
+++ src/heufybot/core/Logger.java
@@ -1,10 +1,25 @@
 package heufybot.core;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import heufybot.utils.FileUtils;
+
 public class Logger 
 {
 	public static void log(String line)
 	{
+		//Timestamp line
+		DateFormat dateFormat = new SimpleDateFormat("[HH:mm]");
+		Date date = new Date();
+		line = dateFormat.format(date) + " " + line;
+		
+		//Output to console
 		System.out.println(line);
+		
+		//Output to logfile
+		FileUtils.writeFileAppend("test.log", line + "\n");
 	}
 	
 	public static void error(String errorSource, String line)
