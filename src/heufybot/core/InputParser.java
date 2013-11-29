5136707ef4331d5fe6b44c2e42a331bf56f42f3e
Heufneutje <stefan.frijters@hotmail.com>
2013-11-30 00:46:59 +0100

    Stop doing the formatting wrong

--- src/heufybot/core/InputParser.java
+++ src/heufybot/core/InputParser.java
@@ -33,83 +33,83 @@ public class InputParser
 	public void parseLine(String line)
 	{		
 		List<String> parsedLine = MessageUtils.tokenizeLine(line);
-
-        String senderInfo = "";
-        if (parsedLine.get(0).charAt(0) == ':')
-                senderInfo = parsedLine.remove(0);
-        
-        String command = parsedLine.remove(0).toUpperCase();
-        
-        if (command.equals("PING"))
-        {
-            irc.cmdPONG(parsedLine.get(0));
-        	return;
-        }
-        else if(command.startsWith("ERROR"))
-        {
-        	//Connection closed by server
-        	Logger.log(parsedLine.get(0));
-        	irc.disconnect();
-        	return;
-        }
-        
-        String sourceNick;
-        String sourceLogin = "";
-        String sourceHostname = "";
-        String target = !parsedLine.isEmpty() ? parsedLine.get(0) : "";
-        
-        if(target.startsWith(":"))
-        {
-        	target = target.substring(1);
-        }
-        
-        int exclamationPosition = senderInfo.indexOf("!");
-        int atPosition = senderInfo.indexOf("@");
-        
-        if (senderInfo.startsWith(":"))
-        {
-	        if(exclamationPosition > 0 && atPosition > 0 && exclamationPosition < atPosition)
-	        {
-	        	//This is a command
-	        	sourceNick = senderInfo.substring(1, exclamationPosition);
-	        	sourceLogin = senderInfo.substring(exclamationPosition + 1, atPosition);
-	        	sourceHostname = senderInfo.substring(atPosition + 1);
-	        }
-	        else
-	        {
-	        	//This is probably a server response
-	        	int code = ParsingUtils.tryParseInt(command);
-	        	if(code != -1)
-	        	{
-	        		if(irc.getConnectionState() != ConnectionState.Connected)
-	        		{
-	        			handleConnect(line, parsedLine, command);
-	        		}
-	        		else
-	        		{
-	        			handleServerResponse(line, parsedLine, command);
-	        		}
-	        		return;
-	        	}
-	        	else
-	        	{
-	        		//Could be a nick without a login or host
-	            	sourceNick = senderInfo;
-	        	}
-	        }
-        }
-        else
-        {
-        	//No idea what this is
-    		return;
-        }
-        
-        if(sourceNick.startsWith(":"))
-        {
-        	sourceNick = sourceNick.substring(1);
-        }
-        
-        handleCommand(line, parsedLine, sourceNick, sourceLogin, sourceHostname, command, target);
+		
+		String senderInfo = "";
+		if (parsedLine.get(0).charAt(0) == ':')
+		        senderInfo = parsedLine.remove(0);
+		
+		String command = parsedLine.remove(0).toUpperCase();
+		
+		if (command.equals("PING"))
+		{
+		    irc.cmdPONG(parsedLine.get(0));
+			return;
+		}
+		else if(command.startsWith("ERROR"))
+		{
+			//Connection closed by server
+			Logger.log(parsedLine.get(0));
+			irc.disconnect();
+			return;
+		}
+		
+		String sourceNick;
+		String sourceLogin = "";
+		String sourceHostname = "";
+		String target = !parsedLine.isEmpty() ? parsedLine.get(0) : "";
+		
+		if(target.startsWith(":"))
+		{
+			target = target.substring(1);
+		}
+		
+		int exclamationPosition = senderInfo.indexOf("!");
+		int atPosition = senderInfo.indexOf("@");
+		
+		if (senderInfo.startsWith(":"))
+		{
+		    if(exclamationPosition > 0 && atPosition > 0 && exclamationPosition < atPosition)
+		    {
+		    	//This is a command
+		    	sourceNick = senderInfo.substring(1, exclamationPosition);
+		    	sourceLogin = senderInfo.substring(exclamationPosition + 1, atPosition);
+		    	sourceHostname = senderInfo.substring(atPosition + 1);
+		    }
+		    else
+		    {
+		    	//This is probably a server response
+		    	int code = ParsingUtils.tryParseInt(command);
+		    	if(code != -1)
+		    	{
+		    		if(irc.getConnectionState() != ConnectionState.Connected)
+		    		{
+		    			handleConnect(line, parsedLine, command);
+		    		}
+		    		else
+		    		{
+		    			handleServerResponse(line, parsedLine, command);
+		    		}
+		    		return;
+		    	}
+		    	else
+		    	{
+		    		//Could be a nick without a login or host
+		        	sourceNick = senderInfo;
+		    	}
+		    }
+		}
+		else
+		{
+			//No idea what this is
+			return;
+		}
+		
+		if(sourceNick.startsWith(":"))
+		{
+			sourceNick = sourceNick.substring(1);
+		}
+		
+		handleCommand(line, parsedLine, sourceNick, sourceLogin, sourceHostname, command, target);
 	}
 	
 	public void handleConnect(String line, List<String> parsedLine, String code)
