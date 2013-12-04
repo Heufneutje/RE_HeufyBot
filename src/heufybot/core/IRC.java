811c415492bfa221d7471b5257ba49da517ef82e
Heufneutje <stefan.frijters@hotmail.com>
2013-12-04 22:09:26 +0100

    Attempt to fix the socket EOF bug

--- src/heufybot/core/IRC.java
+++ src/heufybot/core/IRC.java
@@ -75,8 +75,8 @@ public class IRC
 		try
 		{
 			this.socket = new Socket(server, port);
-			this.inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.defaultCharset()));
-			this.outputWriter = new OutputStreamWriter(socket.getOutputStream(), Charset.defaultCharset());
+			this.inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
+			this.outputWriter = new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8"));
 			Logger.log("*** Connected to the server.");
 			return true;
 		}
