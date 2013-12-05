7d2f147d3f75a7596dc4df80653ffcf38a97720b
Heufneutje <stefan.frijters@hotmail.com>
2013-12-05 22:44:34 +0100

    Add a Choose module

---
+++ modules/Choose/src/heufybot/modules/Choose.java
@@ -0,0 +1,61 @@
+package heufybot.modules;
+
+import heufybot.utils.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Choose extends Module
+{
+	public Choose()
+	{
+		this.authType = Module.AuthType.Anyone;
+		this.trigger = "^" + commandPrefix + "(choose)($| .*)";
+	}
+	
+	@Override
+	public void processEvent(String source, String message, String triggerUser, List<String> params) 
+	{
+		if (params.size() == 1)
+		{
+			bot.getIRC().cmdPRIVMSG(source, "Choose what?");
+		}
+		else
+		{
+			params.remove(0);
+			List<String> choices;
+			
+			if(message.contains(","))
+			{
+				choices = StringUtils.parseStringtoList(StringUtils.join(params, " "), ",");
+			}
+			else if(message.contains(" "))
+			{
+				choices = params;
+			}
+			else
+			{
+				choices = new ArrayList<String>();
+				choices.add(params.get(0));
+			}
+			
+			int choiceNumber = (int) (Math.random() * choices.size());
+			bot.getIRC().cmdPRIVMSG(source, "Choice: " + choices.get(choiceNumber));
+		}
+	}
+
+	public String getHelp()
+	{
+		return "Commands: " + commandPrefix + "choose <choice1>, <choice2> | Makes the bot choose one of the given options at random.";
+	}
+
+	@Override
+	public void onLoad() 
+	{
+	}
+
+	@Override
+	public void onUnload()
+	{
+	}
+}
