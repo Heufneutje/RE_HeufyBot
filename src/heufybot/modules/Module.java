779b4b0134700158e7a7f92abcbaa8170d2a5af9
Bjørn Snoen <bjorn.snoen@gmail.com>
2013-12-05 17:37:43 +0100

    Added javadoc to the Module class.

--- src/heufybot/modules/Module.java
+++ src/heufybot/modules/Module.java
@@ -4,16 +4,45 @@ import java.util.List;
 
 import heufybot.core.HeufyBot;
 
+/**
+ * Abstract Module class.
+ * All modules must extend this class in order to be loadable.
+ * @author Heufneutje
+ * @version 0.0.1
+ *
+ */
 public abstract class Module 
 {
+	
 	public enum AuthType
 	{
 		Anyone, OPs
 	}
 	
+	/**
+	 * This variable is to be a regex. An example of a trigger set in the constructor of a module would be:<br/>
+	 * <i><pre>    this.trigger = "^" + commandPrefix + "(say)($| .*)";</pre></i>
+	 * The trigger is not case sensitive. The above trigger ignores any suffix after the command.
+	 * It is possible to trigger on more than one command, as shown here:
+	 * <i><pre>    this.trigger = "^" + commandPrefix + "(load|unload|reload)($| .*)";</pre></i>
+	 * 
+	 */
 	protected String trigger;
+	/**
+	 * This is set by the abstract constructor, and must be used in the trigger.
+	 * It is what users will input before a command in order for the bot to recognize it as a command.
+	 */
 	protected String commandPrefix;
+	/**
+	 * A reference to the static {@link HeufyBot} instance.
+	 */
 	protected HeufyBot bot;
+	/**
+	 * Who can invoke the module.
+	 * If Anyone is used, any IRC user is allowed to invoke the module.
+	 * If OPs is used, only channel operators are allowed to invoke the module.
+	 *
+	 */
 	protected AuthType authType;
 	
 	public Module()
@@ -22,6 +51,13 @@ public abstract class Module
 		commandPrefix = bot.getConfig().getCommandPrefix();
 	}
 	
+	/**
+	 * This method gets called when the module command triggers.
+	 * @param source The name of the channel from which the command originated.
+	 * @param message The message received, with any command, without username or channel information.
+	 * @param triggerUser The IRC username that entered the command
+	 * @param params The same as message, split at " ". The first index can be checked for the trigger.
+	 */
 	public abstract void processEvent(String source, String message, String triggerUser, List<String> params);
 	
 	public String getTrigger()
@@ -33,10 +69,21 @@ public abstract class Module
 	{
 		return authType;
 	}
-	
+	/**
+	 * Every module can be checked for syntax by the help module. This is the method that gets called to facilitate that.
+	 * @return The string to be printed by the help module when a user calls (commandPrefix)help yourmodule
+	 */
 	public abstract String getHelp();
 	
+	/**
+	 * Gets called when the module is loaded.
+	 * Use this to make sure any necessary resources exist, such as folders or files.
+	 */
 	public abstract void onLoad();
 	
+	/**
+	 * Use this to destroy any resources no longer needed after the module is unloaded.
+	 * Do not use this to destroy resources that would be recreated by onLoad().
+	 */
 	public abstract void onUnload();
 }
