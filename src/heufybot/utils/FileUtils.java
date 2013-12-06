5fd62e954cbe4205a5843597e5d88fa809ef4d9e
Stefan Frijters <stefan.frijters@hotmail.com>
2013-12-06 14:07:19 +0100

    Don't really need that there anymore

--- src/heufybot/utils/FileUtils.java
+++ src/heufybot/utils/FileUtils.java
@@ -94,7 +94,6 @@ public class FileUtils
 		}
 		catch (IOException e)
 		{
-			e.printStackTrace();
 			Logger.error("FileUtils", "The file " + filePath + " could not be written.");
 			return false;
 		}
