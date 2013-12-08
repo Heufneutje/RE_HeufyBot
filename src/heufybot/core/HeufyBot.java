f47450f02f3928968f5a9f066150a592cacab5e5
Heufneutje <stefan.frijters@hotmail.com>
2013-12-08 14:17:27 +0100

    Bump version

--- src/heufybot/core/HeufyBot.java
+++ src/heufybot/core/HeufyBot.java
@@ -9,7 +9,7 @@ import heufybot.utils.enums.ModuleLoaderResponse;
 
 public class HeufyBot
 {
-	public final static String VERSION = "0.1.0";
+	public final static String VERSION = "0.2.0";
 	private Config config;
 	private IRC irc;
 	private ModuleInterface moduleInterface;
