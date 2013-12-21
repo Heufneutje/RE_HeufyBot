7b37cc75470480fce1e2d9b746330f9a72e06620
Heufneutje <stefan.frijters@hotmail.com>
2013-12-21 12:39:35 +0100

    Add a new Weather module

--- modules/Weather/src/heufybot/modules/Weather.java
+++ modules/Weather/src/heufybot/modules/Weather.java
@@ -1,18 +1,24 @@
 package heufybot.modules;
 
 import heufybot.utils.FileUtils;
-import heufybot.utils.URLUtils;
+import heufybot.utils.StringUtils;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.simple.parser.ParseException;
 
 public class Weather extends Module 
 {
+	private HashMap<String, String> userLocations;
+	private final String locationsPath = "data/userlocations.txt";
+	
 	public Weather()
 	{
 		this.authType = Module.AuthType.Anyone;
-		this.trigger = "^" + commandPrefix + "(weather|forecast)($| .*)";
+		this.trigger = "^" + commandPrefix + "(weather|forecast|registerloc)($| .*)";
+		
+		this.userLocations = new HashMap<String, String>();
 	}
 
 	@Override
@@ -23,12 +29,45 @@ public class Weather extends Module
 			bot.getIRC().cmdPRIVMSG(source, "No WorldWeatherOnline API key found");
 			return;
 		}
+		
+		if(message.matches("^" + commandPrefix + "registerloc.*"))
+		{
+			if(params.size() == 1)
+			{
+				bot.getIRC().cmdPRIVMSG(source, "You didn't give a location to register.");
+				return;
+			}
+			else
+			{
+				params.remove(0);
+				String location = StringUtils.join(params, " ").replaceAll("=", "");
+				boolean alreadyRegistered = false;
+				
+				if(userLocations.containsKey(triggerUser))
+				{
+					alreadyRegistered = true;
+				}
+				
+				userLocations.put(triggerUser, location);
+				writeLocations();
+				
+				if(alreadyRegistered)
+				{
+					bot.getIRC().cmdPRIVMSG(source, "Your location has been updated.");
+				}
+				else
+				{
+					bot.getIRC().cmdPRIVMSG(source, "Your location is now registered.");
+				}
+				return;
+			}
+		}
 
 		if (params.size() == 1)
 		{
-			if(URLUtils.grab("http://tsukiakariusagi.net/chatmaplookup.php?nick=" + triggerUser).equals(", "))
+			if(!userLocations.containsKey(triggerUser))
 			{
-				bot.getIRC().cmdPRIVMSG(source, "You are not registered on the chatmap.");
+				bot.getIRC().cmdPRIVMSG(source, "You are not registered. Use \"" + commandPrefix + "registerloc <location>\" to register your location.");
 				return;
 			}			
 			params.add(triggerUser);
@@ -76,7 +115,7 @@ public class Weather extends Module
 
 		try
 		{
-			Geolocation location = geo.getGeolocationForIRCUser(params.get(0));
+			Geolocation location = geo.getGeolocationForPlace(userLocations.get(params.get(0)));
 			if (location != null)
 			{
 				String loc = location.locality;
@@ -163,6 +202,29 @@ public class Weather extends Module
 		String forecast = weatherInterface.getForecast(location.latitude, location.longitude);
 		return forecast;
 	}
+	
+	private void writeLocations()
+	{
+		String result = "";
+		for(String user : userLocations.keySet())
+		{
+			result += user + "=" + userLocations.get(user);
+		}
+		FileUtils.writeFile(locationsPath, result);
+	}
+	
+	private void readLocations()
+	{
+		String[] locationArray = FileUtils.readFile(locationsPath).split("\n");
+		if(locationArray[0].length() > 0)
+		{
+			for(int i = 0; i < locationArray.length; i++)
+			{
+				String[] location = locationArray[i].split("=");
+				userLocations.put(location[0], location[1]);
+			}
+		}
+	}
 
 	@Override
 	public String getHelp(String message) 
@@ -174,10 +236,14 @@ public class Weather extends Module
 	public void onLoad() 
 	{
 		FileUtils.touchFile("data/worldweatheronlineapikey.txt");
+		FileUtils.touchFile(locationsPath);
+		
+		readLocations();
 	}
 
 	@Override
 	public void onUnload() 
 	{
+		writeLocations();
 	}
 }
