cf3215af787d80fdb072fa32c67478872edb22ce
Heufneutje <stefan.frijters@hotmail.com>
2013-12-27 23:42:57 +0100

    Add Imgur support to URLFollow

--- modules/URLFollow/src/heufybot/modules/URLFollow.java
+++ modules/URLFollow/src/heufybot/modules/URLFollow.java
@@ -1,18 +1,24 @@
 package heufybot.modules;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import heufybot.utils.FileUtils;
+import heufybot.utils.StringUtils;
 import heufybot.utils.URLUtils;
 
 public class URLFollow extends Module
 {
+	private final String imgurClientIDPath = "data/imgurclientid.txt";
+	
 	public URLFollow()
 	{
 		this.authType = AuthType.Anyone;
@@ -41,23 +47,34 @@ public class URLFollow extends Module
 			String fullHostname = URLUtils.getFullHostname(urlstring);
 			if(urlstring.toLowerCase().contains("imgur.com"))
 			{
-				
+				if(FileUtils.readFile(imgurClientIDPath).equals(""))
+				{
+					bot.getIRC().cmdPRIVMSG(source, followNormalURL(urlstring));
+					return;
+				}
+				else
+				{
+					Pattern p = Pattern.compile("imgur\\.com/([a-zA-Z0-9/]*)");
+					Matcher m = p.matcher(urlstring);
+					if(m.find())
+					{
+						bot.getIRC().cmdPRIVMSG(source, followImgur(m.group(1)));
+					}
+				}
 			}
 			else if(!urlstring.toLowerCase().matches(".*(jpe?g|gif|png|bmp)"))
 			{
 				if(fullHostname != null)
 				{
-					Pattern p = Pattern.compile("youtube.com/watch\\?v=([a-zA-Z0-9_]*)");
+					Pattern p = Pattern.compile("youtube\\.com/watch\\?v=([a-zA-Z0-9_]*)");
 					Matcher m = p.matcher(fullHostname);
 					
 					if(m.find())
 					{
 						bot.getIRC().cmdPRIVMSG(source, followYouTubeURL(m.group(1)));
+						return;
 					}
-					else
-					{
-						bot.getIRC().cmdPRIVMSG(source, followNormalURL(urlstring));
-					}
+					bot.getIRC().cmdPRIVMSG(source, followNormalURL(urlstring));
 				}
 			}
 		}
@@ -119,8 +136,78 @@ public class URLFollow extends Module
 		} 
 		catch (ParseException e) 
 		{
-			e.printStackTrace();
-			return null;
+			return "Something went wrong while parsing the data";
+		}
+	}
+	
+	private String followImgur(String imgurID)
+	{
+		String url = "";
+		boolean isAlbum = false;
+		
+		if(imgurID.startsWith("gallery/"))
+		{
+			imgurID = imgurID.replaceAll("gallery/", "");
+			url = "https://api.imgur.com/3/gallery/" + imgurID;
+		}
+		else if(imgurID.startsWith("a/"))
+		{
+			imgurID = imgurID.replaceAll("a/", "");
+			isAlbum = true;
+			url = "https://api.imgur.com/3/album/" + imgurID;
+		}
+		else
+		{
+			url = "https://api.imgur.com/3/image/" + imgurID;
+		}
+		
+		try 
+		{
+			JSONObject data = (JSONObject) getImgurJSON(url).get("data");
+			List<String> imageData = new ArrayList<String>();
+			
+			if(data.get("title") == null)
+			{
+				imageData.add("No Title");
+			}
+			else
+			{
+				imageData.add("Title: " + data.get("title").toString());
+			}
+			
+			if(data.get("nsfw") != null)
+			{
+				imageData.add("NSFW!");
+			}
+			
+			if(isAlbum)
+			{
+				imageData.add("Album, " + data.get("images_count").toString() + " images");
+			}
+			else
+			{
+				if(data.containsKey("is_album") && data.get("is_album") != null)
+				{
+					JSONArray images = (JSONArray) data.get("images");
+					imageData.add("Album, " + images.size() + " images");
+				}
+				else
+				{
+					if(data.get("animated").toString().equals("true"))
+					{
+						imageData.add("Animated");
+					}
+					imageData.add("Dimensions: " + data.get("width").toString() + "x" + data.get("height").toString());
+					imageData.add("File size: " + StringUtils.tryParseInt(data.get("size").toString()) / 1024 + " kB");
+				}
+			}
+			imageData.add("Views: " + data.get("views").toString());
+			
+			return StringUtils.join(imageData, " | ");
+		}
+		catch (ParseException e) 
+		{
+			return "Something went wrong while parsing the data";
 		}
 	}
 	
@@ -129,9 +216,17 @@ public class URLFollow extends Module
 		return (JSONObject)new JSONParser().parse(URLUtils.grab(urlString));
 	}
 	
+	private JSONObject getImgurJSON(String urlString) throws ParseException
+	{
+		HashMap<String, String> headers = new HashMap<String, String>();
+		headers.put("Authorization", "Client-ID " + FileUtils.readFile(imgurClientIDPath).replaceAll("\n", ""));
+		return (JSONObject)new JSONParser().parse(URLUtils.grab(urlString, headers));
+	}
+	
 	@Override
 	public void onLoad()
 	{
+		FileUtils.touchFile(imgurClientIDPath);
 	}
 
 	@Override
