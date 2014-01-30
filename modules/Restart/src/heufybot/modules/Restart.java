0b4aad6a23fae07c465e26d1d1937e9e096f80ae
Heufneutje <stefan.frijters@hotmail.com>
2014-01-30 21:27:44 +0100

    Add a Restart module

---
+++ modules/Restart/src/heufybot/modules/Restart.java
@@ -0,0 +1,34 @@
+package heufybot.modules;
+
+import java.util.List;
+
+public class Restart extends Module
+{
+	public Restart()
+	{
+		this.authType = Module.AuthType.OPs;
+		this.triggerTypes = new TriggerType[] { TriggerType.Message };
+		this.trigger = "^" + commandPrefix + "(restart)$";
+	}
+	
+	@Override
+	public void processEvent(String source, String message, String triggerUser, List<String> params) 
+	{
+		bot.restart();
+	}
+
+	public String getHelp(String message)
+	{
+		return "Commands: " + commandPrefix + "restart | Makes the reload its config and modules and reconnect to the server.";
+	}
+
+	@Override
+	public void onLoad() 
+	{
+	}
+
+	@Override
+	public void onUnload()
+	{
+	}
+}
