d6c2bb0420ff266b2c4094471f37f5102551f912
Didero <flabberdewaps@gmail.com>
2014-01-30 17:20:18 +0100

    Made 'Hide Events' link work with GET now
    Also added some data validation. Better safe than sorry.

--- www/loggrab.php
+++ www/loggrab.php
@@ -7,8 +7,15 @@
 <script src="eventsscript.js"></script>
 <title>
 <?php
-if (isset($_GET['channel']) and isset($_GET['date'])) {
-	echo 'Log for #'.$_GET['channel'].' of '.$_GET['date'];
+//First and foremost, some input validation
+$channel = (isset($_GET['channel']) ? htmlspecialchars($_GET['channel']) : FALSE) ;
+$date = (preg_match('/\A\d{4}-\d{2}-\d{2}\z/', $_GET['date']) === 1 ? htmlspecialchars($_GET['date']) : FALSE);
+$hideEvents = FALSE;
+if ($_GET['hideevents'] === 'true') $hideEvents = TRUE;
+
+if ($channel !== FALSE and $date !== FALSE) {
+	echo 'Log for #'.$channel.' of '.$date;
+	if ($hideEvents) echo ' with events hidden';
 }
 else echo 'Log Prettifier';
 ?>
@@ -16,17 +23,26 @@ else echo 'Log Prettifier';
 </head>
 <body>
 <?php
-if (!isset($_GET['channel'])) echo 'No channel name provided!';
-elseif(!isset($_GET['date'])) echo 'No date provided!';
+if ($channel === FALSE) echo 'No channel name provided!';
+elseif($date === FALSE) echo 'No date provided!';
 else {
-	$filename = '/home/stefan/heufybot-desertbus/logs/DesertBusForHope/#'.$_GET['channel'].'/'.$_GET['date'].'.log';
+	$filename = '/home/stefan/heufybot-desertbus/logs/DesertBusForHope/#'.$channel.'/'.$date.'.log';
 	$log = file_get_contents($filename);
 	if ($log === FALSE) echo 'Error while trying to open file "'.$filename.'"';
 	else {
 		unset($filename);
 		$lines = explode("\n", htmlspecialchars($log));
 		unset($log);
-		echo '<span><a href="#" id="eventToggle" onclick="toggleEventVisibility(true); return false;">Hide events</a></span>'."\r\n";
+		
+		$eventLinkUrl = $_SERVER['REQUEST_URI'];
+		//If there's already a hideevents setting, replace that
+		if (strpos($eventLinkUrl, 'hideevents') !== FALSE) {
+			$eventLinkUrl = preg_replace('/hideevents=[^&\z]+/', 'hideevents='.($hideEvents?'false':'true'), $eventLinkUrl);
+		}
+		//otherwise, add it on
+		else $eventLinkUrl .= '&hideevents='.($hideEvents?'false':'true');
+		echo '<span><a href="'.$eventLinkUrl.'">'.($hideEvents?'Show':'Hide').' events</a></span>'."\r\n";
+		
 		echo '<table class="log" id="log"><tr class="message"> <th class="time">TIME</th> <th class="user">NICK</th> <th class="text">MESSAGE</th></tr>'."\r\n";
 		$timestampLength = 7;
 		$suffixCharactersToRemove = array(')', '.');
@@ -41,12 +57,14 @@ else {
 					else $messageType = 'other';
 					$nickType = 'spacer';
 				}
+				//If we should hide events, just skip the echo-ing when it's a join or quit
+				if ($hideEvents and $messageType === 'other') {
+					continue;
+				}
 				$message = substr($line, $timestampLength + strlen($lineSections[1])+2);
 				//Turn URLs into hyperlinks
 				if (strpos($message, 'http') !== FALSE or strpos($message, 'www') !== FALSE) {
-					//$message = preg_replace("/(https?:\/\/[\S]+)/", "<a href=\"$0\">$0</a>", $message);
 					preg_match_all("/(https?:\/\/\S+|www\.\S+\.\S+)/", $message, $regexResults);
-					//$message = '['.count($regexResults[0]).'] '.$message;
 					foreach ($regexResults[0] as $urlText) {
 						//Remove some trailing characters that can mess up the url, like periods or parentheses						
 						while (in_array(mb_substr($urlText, -1, 1), $suffixCharactersToRemove)) {
