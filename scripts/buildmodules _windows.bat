a54e839d43cf0bdb535eb3b9b9645ca38c95bd60
Heufneutje <stefan.frijters@hotmail.com>
2014-03-23 13:52:13 +0100

    Add scripts to wrap all compiled modules into JAR files
    Fixes #27

---
+++ scripts/buildmodules _windows.bat
@@ -0,0 +1,29 @@
+cd ..\modules
+jar cvf Aww.jar Aww\bin\*
+jar cvf Choose.jar Choose\bin\*
+jar cvf Do.jar Do\bin\*
+jar cvf Join.jar Join\bin\*
+jar cvf Log.jar Log\bin\*
+jar cvf LogEC2.jar LogEC2\bin\*
+jar cvf Nick.jar Nick\bin\*
+jar cvf NowPlaying.jar NowPlaying\bin\*
+jar cvf OutOfContext.jar OutOfContext\bin\*
+jar cvf Part.jar Part\bin\*
+jar cvf Quit.jar Quit\bin\*
+jar cvf Restart.jar Restart\bin\*
+jar cvf Say.jar Say\bin\*
+jar cvf Shorten.jar Shorten\bin\*
+jar cvf Source.jar Source\bin\*
+jar cvf Tell.jar Tell\bin\*
+jar cvf Time.jar Time\bin\*
+jar cvf TimeDB.jar TimeDB\bin\*
+jar cvf Translate.jar Translate\bin\*
+jar cvf TranslationParty.jar TranslationParty\bin\*
+jar cvf Uptime.jar Uptime\bin\*
+jar cvf URLFollow.jar URLFollow\bin\*
+jar cvf UserLocation.jar UserLocation\bin\*
+jar cvf Weather.jar Weather\bin\*
+jar cvf WeatherDB.jar WeatherDB\bin\*
+jar cvf Youtube.jar YouTube\bin\*
+pause
+exit
